076efc2c8d18f709e2cc3b9ba7844eeb3670e098
Ignacio Mondino <ignacio.mondino@gmail.com>
2024-07-13 22:14:27 -0300

    First push

---
+++ src/components/DolarBlue.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function DolarBlue() {
+  const [valorVenta, setValorVenta] = useState(null);
+  const [valorCompra, setValorCompra] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.bluelytics.com.ar/v2/latest")
+      .then((response) => response.json())
+      .then((data) => {
+        setValorVenta(data.blue["value_sell"]);
+        setValorCompra(data.blue["value_buy"]);
+      });
+  }, []);
+
+  return (
+    <div className="container text-center mt-5 dolar-card">
+      <h1 style={{ color: "#000080" }}>Dolar Blue</h1>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="row">
+          <div className="col">
+            <h1>${valorCompra}</h1>
+            <p className="fs-4 fw-bold">Compra</p>
+          </div>
+          <div className="col">
+            <h1>${valorVenta}</h1>
+            <p className="fs-4 fw-bold">Venta</p>
+            <div className="col">
+            <h1>${ (valorVenta + valorCompra) / 2}</h1>
+            <p className="fs-4 fw-bold">Valor Medio</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DolarBlue;
